ad27692c8606de821f5b56b1664fa1e2f49d4e3c
CharHer1221 <144883937+CharHer1221@users.noreply.github.com>
2023-10-12 15:11:23 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus")
+document.addEventListener("keydown", function (event) { 
+    if (event) {
+        startGame();
+    }
+});
+function jump() {
+    if (dino.classList != "jump"){
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+        dino.classList.remove("jump");
+    }, 300);
+}
+}
+
+function startGame() {
+    jump();
+
+}
+let isAlive = setInterval(function () {
+    
+    //get current dino position on y axis
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+    //get current cactus position on x axis
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    
+    //detect collision
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+        window.open('gameOver.html', '_self');
+    }
+}, 10);
+
+
+
+
